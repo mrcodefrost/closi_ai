13619602f28b15d68525500331fa3f0961e7ff4d
Daksh <72447688+mrcodefrost@users.noreply.github.com>
2025-09-15 03:49:31 +0530

    Profile screen UI TBD

--- lib/features/profile/views/profile_screen.dart
+++ lib/features/profile/views/profile_screen.dart
@@ -1,5 +1,7 @@
-import 'package:ai_closet_flutter/features/common/nested_app_bar.dart';
+import 'package:ai_closet_flutter/core/constants/app_images.dart';
+import 'package:ai_closet_flutter/core/theme/color_constants.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -7,10 +9,92 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: NestedAppBar(title: NestedAppBarTitles.myCloset),
-      body: Column(children: []),
-      floatingActionButton: FloatingActionButton(onPressed: () {}),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+            children: [
+              Row(
+                spacing: 10,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    icon: Icon(Icons.arrow_back_outlined),
+                    style: IconButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      backgroundColor: IconColor.white,
+                      foregroundColor: IconColor.brown,
+                    ),
+                  ),
+
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Profile',
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      Text(
+                        'Manage your account',
+                        style: Theme.of(context).textTheme.bodySmall,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: BackgroundColor.offWhite,
+                ),
+                child: Row(
+                  children: [
+                    const CircleAvatar(
+                      radius: 35,
+                      backgroundImage: AssetImage(AppImages.onboardingOne),
+                    ),
+                    const SizedBox(width: 10),
+
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        spacing: 10,
+                        children: [
+                          Text(
+                            'Aarya Bhantia',
+                            style: Theme.of(context).textTheme.bodyLarge
+                                ?.copyWith(fontWeight: FontWeight.w700),
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                          ),
+                          Text(
+                            'aryatangsme@gmail.com',
+                            style: Theme.of(context).textTheme.bodySmall,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    const SizedBox(width: 10),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.edit, size: 25, color: IconColor.brown),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
